84fb33a53f5e8bbb0992adaeebc163d7e5291762
RazielPendragon <sebastian.ponce12@inacapmail.cl>
2022-04-14 20:59:45 -0400

    arreglo

--- scripts/insertar tabla usuario restaurante.sql
+++ scripts/insertar tabla usuario restaurante.sql
@@ -1,4 +1,4 @@
-INSERT INTO bitnami_myapp.usuarios (usuario_correo,usuario_nombre_completo) VALUES
+INSERT INTO restaurantes.usuarios (usuario_correo,usuario_nombre_completo) VALUES
 	 ('piccola@gmail.com','Piccola Italia'),
 	 ('macdonalds@gmail.com','Macdonalds'),
 	 ('tarragona@gmail.com','Tarragona');
