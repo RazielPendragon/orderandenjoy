cc1c369dbf9784f35b76f195f8a876c13aeafa51
RazielPendragon <sebastian.ponce12@inacapmail.cl>
2022-04-19 19:14:18 -0400

    Arreglo menor Scripts

--- scripts/005_crear_tabla_mesa.sql
+++ scripts/005_crear_tabla_mesa.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `Mesa` (
   `mesa_id` int(100) NOT NULL,
-  `n° mesa` int(100) DEFAULT NULL,
+  `n_mesa` int(100) DEFAULT NULL,
   `estado_mesa` varchar(100) DEFAULT NULL,
   `capacidad_mesa` int(20) DEFAULT NULL,
   `restaurantes_id` int(11) NOT NULL,
--- scripts/006_disponibilidad_mesas.sql
+++ scripts/006_disponibilidad_mesas.sql
@@ -1,11 +1,11 @@
 INSERT INTO restaurantes.Mesa 
-(mesa_id,`n° mesa`, estado_mesa , restaurantes_id, capacidad_mesa,dia_reserva)
+(mesa_id, n_mesa, estado_mesa , restaurantes_id, capacidad_mesa, dia_reserva)
 VALUES
 (1,1, 'Reservada',1, 2, '2022-04-23'), 
 (2,2, 'Disponible',1, 2, '2022-04-23'),
 (3,3, 'Reservada',1, 2,'2022-04-23'),
 (4,4, 'Disponible',1, 2,'2022-04-23'),
-(5,5, 'Reservada',1, 6,'2022-04-23')
+(5,5, 'Reservada',1, 6,'2022-04-23'),
 (1,1, 'Disponible',1, 2, '2022-04-24'), 
 (2,2, 'Reservada',1, 2, '2022-04-24'),
 (3,3, 'Diponible',1, 2,'2022-04-24'),
