85fcf1f1a9c9617c77456d3adc9c5b3c3b4445cd
FranciscaLopezz <102864406+FranciscaLopezz@users.noreply.github.com>
2022-04-21 23:54:52 -0400

    avance pequeño agregar mesas

--- my-project/myapp/app/Views/MesaRestaurante/agregarFormulario.php
+++ my-project/myapp/app/Views/MesaRestaurante/agregarFormulario.php
@@ -1,3 +1,26 @@
 estoy agregando 
 
 <a href="<?php echo site_url('/mesaRestaurante-agregarBaseDatos/')?>">Agregar</a>
+
+<?php session_start(); ?>
+<?= $this->extend('formaStandar') ?>
+
+<?= $this->section('contenido') ?>
+  <div class="container mt-5">
+    <form method="post" id="add_create" name="add_create" action="<?= site_url('/menu-agregar-continuar') ?>">
+      <div class="form-group">
+        <label>N° mesa</label>
+        <input type="text" name="n° mesa" class="form-control"/>
+      </div>
+      <div class="form-group">
+        <label>Estado mesa</label>
+        <input type="text" name="estado" class="form-control"/>
+      </div>
+      <div class="form-group">
+        <label>Capacidad</label>
+        <input type="text" name="capacidad" class="form-control"/>
+      </div>
+      <div class="form-group">
+        <label>Dia reserva</label>
+        <input type="text" name="dia reserva" class="form-control"/>
+      </div>
