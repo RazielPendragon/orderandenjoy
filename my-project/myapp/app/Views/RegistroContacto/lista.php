d805589b2b46f8ff90aaa08ec7b541bfdf13b658
FranciscaLopezz <102864406+FranciscaLopezz@users.noreply.github.com>
2022-05-19 00:04:36 -0400

    arreglo botones

--- my-project/myapp/app/Views/RegistroContacto/lista.php
+++ my-project/myapp/app/Views/RegistroContacto/lista.php
@@ -3,7 +3,7 @@
 <?= $this->section('contenido') ?>
 <div class="container center text-center">
 <!-- ARREGLAR -->
-<form method="post" id="add_create" name="add_create" action="<?= site_url('/Contacto-Registro') ?>">
+<form method="get" id="add_create" name="add_create" action="<?= site_url('/registro-cancelarContacto') ?>">
 <h1>¡Registro exitoso!, espera a que te contactemos</h1>
 <button type="submit" class="btn btn-primary btn-sm">volver</button>
 </form>
--- my-project/myapp/app/Views/RegistroRestaurante/lista.php
+++ my-project/myapp/app/Views/RegistroRestaurante/lista.php
@@ -2,7 +2,7 @@
 
 <?= $this->section('contenido') ?>
 <div class="container center text-center">
-<form method="post" id="add_create" name="add_create" action="<?= site_url('/Restaurante-Registro') ?>">
+<form method="get" id="add_create" name="add_create" action="<?= site_url('/Restaurante-Registro') ?>">
 <h1>¡Registro exitoso!, recuerda enviar los datos al Restaurante</h1>
 <button type="submit" class="btn btn-primary btn-sm">Crear otro registro</button>
 </form>
