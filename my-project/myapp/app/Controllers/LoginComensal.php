7b38cee2f23a5c13e2f6b1bd83754608d560b6ac
RazielPendragon <sebastian.ponce12@inacapmail.cl>
2022-05-20 15:41:21 -0400

    Arreglo comensales

--- my-project/myapp/app/Controllers/LoginComensal.php
+++ my-project/myapp/app/Controllers/LoginComensal.php
@@ -4,11 +4,24 @@ namespace App\Controllers;
 
 use App\Models\RegistroComensalModel;
 use App\Models\UsuarioComensalModel;
+use App\Models\MenuModel;
+use App\Models\UsuarioRestauranteModel;
 class LoginComensal extends BaseController
 {
     public function index(){
-        // Pal Home
-        return view('loginComensal/index');
+        // Obtenemos la clase del Model que controla los Restaurantes Y los menus
+        $mod = new UsuarioRestauranteModel(); //mod es la variable a modelo
+        $mod2 = new MenuModel(); //mod2 es igual al modelo 2
+        // Buscamos los restaurantes con la funcion todos
+        $restaurantes = $mod->todos();
+        // abrimos un foreach donde decimos que
+        foreach ($restaurantes as $r){ //restaurantes es igual a la variable r
+            $menus = $mod2->todEs ($r->id_restaurante); // Busca los platos del restaiurante r
+            $dataRestaurante['restaurante'] = $r; // luego guardamos los datos del restauirante en un mapa
+            $dataRestaurante['menu'] = $menus; // y aqui guardamos los menus en el mismo mapa
+            $data['registros'][] = $dataRestaurante; //y aqui lo agregamos al resulatdo
+        } 
+        return view('loginComensal/index',$data);
     }
     public function login01Formulario()
     {
--- my-project/myapp/app/Views/loginComensal/index.php
+++ my-project/myapp/app/Views/loginComensal/index.php
@@ -1,9 +1,6 @@
+<?php session_start();?>
 <?= $this->extend('/login/formaUsuario') ?>
 <?= $this->section('contenido') ?>
-
-
-
-
 <div class="mt-3">
      <table class="table table-bordered" id="users-list">
        <thead>
@@ -12,22 +9,25 @@
           </tr>
        </thead>
        <tbody>
-       
 		   <?php
-				// Primero se revisa que el arreglo "registros" no este vacio (siempre la BD puede devolver NADA)
-				// Luego apra cada Concierto (reg  --- por registro) en el listado dibujamos la tablita
-				// Brujeria maxima nivel 1: ahcemos link a los ver pero pasando el ID !!!  Ojo que podemos hacerlo igual para modificar y eliminar
-
-				// Recordar que los path de lso links SON los de los Routes no lo de los sistemas operativos
-
-				// Los nombres de lso clases salen de acá
-				// https://getbootstrap.com/docs/4.0/components/buttons/
-				// Simpre respetar usar el mismo foramto
+          foreach ($registros as $registro){
+            $restaurante = $registro['restaurante'];
+            $menus = $registro['menu'];
+            ?>
+            Restaurante: <?= $restaurante->NombreFantasia?><br/>
+            Menu:
+            <?php
+            if ($menus) {}
+              foreach ($menus as $plato) {
+                ?>
+                  <li><?= $plato->nombre ?></li>
+                <?php
+              }
+            }
 		   ?>
        </tbody>
      </table>
   </div>
-
 <div>
 </ul>
    <h1>Comensales</h1>
@@ -35,11 +35,4 @@
        <li>Reservar mesa</li>
    </ul>
 </div>
-
-  <script>
-    $(document).ready( function () {
-      $('#users-list').DataTable();
-  } );
-</script>
-
 <?= $this->endSection() ?>
