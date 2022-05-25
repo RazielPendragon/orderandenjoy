2ba59e6705e67e6096924e05b26bef6aec50507b
nicolesepulvedag <nicole.sepulveda22@inacapmail.cl>
2022-05-24 20:10:54 -0400

    subir imagenes de menu

--- my-project/myapp/app/Views/welcome_message.php
+++ my-project/myapp/app/Views/welcome_message.php
@@ -9,7 +9,7 @@ LANDING PAGE
 	
  <img src="/images/icono reloj.png" alt="">
 	<div class="video-container">
-<!--  <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe> -->
+<!--  <ifram e width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe> -->
  </div>
         
 	
