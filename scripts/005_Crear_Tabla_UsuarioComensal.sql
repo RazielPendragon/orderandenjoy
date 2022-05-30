c98591508747197aa18c2be96a06c6316a7ae497
FranciscaLopezz <102864406+FranciscaLopezz@users.noreply.github.com>
2022-05-30 11:21:12 -0400

    INSERT INTO restaurantes.UsuarioComensal

--- scripts/005_Crear_Tabla_UsuarioComensal.sql
+++ scripts/005_Crear_Tabla_UsuarioComensal.sql
@@ -7,3 +7,8 @@ CREATE TABLE restaurantes.UsuarioComensal(
   usuario_comensal_telefono VARCHAR(300) NOT NULL,
   PRIMARY KEY(usuario_comensal_id)
 );
+
+/* INSERTE SUS DATOS PERSONALES */
+INSERT INTO restaurantes.UsuarioComensal
+(usuario_comensal_nombre_completo, usuario_comensal_correo, usuario_comensal_clave, usuario_comensal_telefono)
+VALUES('Francisca Lopez Valenzuela', 'flopez@gmail.com', 'fran321', '986900552');
